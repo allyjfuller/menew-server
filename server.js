a4c9b5f799d12f67934cc2be419e5c08c5891ec9
allyjfuller <allyjfuller@gmail.com>
2019-04-14 13:22:38 -0400

    git ignore

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
- const app = express();
+const app = express();
 
- const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
  app.get('/api/*', (req, res) => {
    res.json({ok: true});
