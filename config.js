4043d257e11fdbcf7d527d96e4eadbe3f672f2b6
allyjfuller <allyjfuller@gmail.com>
2019-05-07 16:41:14 -0400

    config change

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000'
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/menew-api';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://ally-admin:allyjfuller@menewcluster-vpij5.mongodb.net/test?retryWrites=true';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/menew-api';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret'
